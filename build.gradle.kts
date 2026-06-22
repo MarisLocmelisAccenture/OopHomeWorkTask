0d93d099d016dc05d23b741c1dd30edb4a613509
Maris Locmelis <maris.locmelis@accenture.com>
2026-06-22 10:03:40 +0300

    splitted tests to have tests for all implemented GOF patterns.

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-engine:6.1.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher:6.1.0")
     testImplementation("org.assertj:assertj-core:3.27.7")
+    testImplementation("org.mockito:mockito-core:5.23.0")
 }
 
 tasks.withType<Test> {
--- src/test/java/com/patterns/homework/patterns/GOFPatternTests.java
+++ src/test/java/com/patterns/homework/patterns/GOFPatternTests.java
@@ -1,25 +1,21 @@
 package com.patterns.homework.patterns;
 
-import com.patterns.homework.patterns.member.Bandit;
-import com.patterns.homework.patterns.member.Friend;
-import com.patterns.homework.patterns.member.MafiaBoss;
-import com.patterns.homework.patterns.member.Maris;
+import com.patterns.homework.patterns.dto.FriendWhisperMessage;
+import com.patterns.homework.patterns.dto.LaudMessage;
+import com.patterns.homework.patterns.factory.FactoryMethod;
+import com.patterns.homework.patterns.interfaces.service.MediatorService;
+import com.patterns.homework.patterns.member.*;
+import com.patterns.homework.patterns.service.MessageMediatorServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import com.patterns.homework.patterns.member.AbstractChatMember;
-import com.patterns.homework.patterns.interfaces.service.MediatorService;
-import com.patterns.homework.patterns.factory.FactoryMethod;
-import com.patterns.homework.patterns.service.*;
+import org.mockito.Mockito;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
-class PatternsApplicationTests {
-
+public class GOFPatternTests {
     @Test
-    void contextLoads() {
+    void testMediator() {
         MediatorService mediator = MessageMediatorServiceImpl.getInstance();
         MafiaBoss boss = new MafiaBoss();
 
@@ -52,4 +48,28 @@ class PatternsApplicationTests {
         Assertions.assertThat(marisMessage).doesNotContain("I know");
     }
 
+    @Test
+    void testFactoryMethod() {
+        Assertions.assertThat(FactoryMethod.createFromString("test")).isInstanceOf(LaudMessage.class);
+        Assertions.assertThat(FactoryMethod.createFromString("test", true)).isInstanceOf(FriendWhisperMessage.class);
+    }
+
+    //Wrapper on Bandit to send necessary information to observer.
+    @Test
+    void testObserverAndWrapper() {
+        MafiaBoss boss = new MafiaBoss();
+
+        MediatorService mediatorMock = Mockito.mock(MediatorService.class);
+        AbstractChatMember gangMember = new Bandit(mediatorMock, boss);
+
+        LaudMessage testMessage = new LaudMessage("test message");
+        gangMember.receive(testMessage);
+
+        Assertions.assertThat(boss.getStolenInfo()).contains("test message");
+    }
+
+    @Test
+    void testSingleton() {
+        Assertions.assertThat(MessageMediatorServiceImpl.getInstance()).isInstanceOf(MessageMediatorServiceImpl.class);
+    }
 }
